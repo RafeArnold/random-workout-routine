0c5a46d62badb49ba14522e48907d0bad304a850
Rafe Arnold <rafe.arnold@gmail.com>
2020-04-05 18:00:30 +0100

    Initial Spring project setup

---
+++ src/main/java/net/ddns/rarnold/randomworkoutroutine/RandomWorkoutRoutineApplication.java
@@ -0,0 +1,13 @@
+package net.ddns.rarnold.randomworkoutroutine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomWorkoutRoutineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomWorkoutRoutineApplication.class, args);
+	}
+
+}
