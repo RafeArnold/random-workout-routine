bf31fea4529dbab195118e867fb4181317801c52
Rafe Arnold <rafe.arnold@gmail.com>
2020-04-10 13:29:23 +0100

    Add localStorage functionality and clear button

--- src/index.js
+++ src/index.js
@@ -13,27 +13,48 @@ var routine = [
 
 var currentGroupIndex = 0;
 var setCount = 0;
+var currentExercise;
 
 document.onreadystatechange = function () {
     if (document.readyState === 'complete') {
+        load();
         start();
     }
 };
 
-var start = function () {
-    next();
-    document.onkeypress = next;
+var load = function () {
+    document.onkeypress = function(event) {
+        if (event.code === 'Enter' || event.code === 'Space') {
+            next();
+        }
+    };
     var nextBtn = document.getElementById('nextBtn');
     nextBtn.onclick = function () {
         next();
         nextBtn.blur();
+    };
+    var clearBtn = document.getElementById('clearBtn');
+    clearBtn.onclick = function () {
+        clear();
+        clearBtn.blur();
+    };
+};
+
+var start = function () {
+    if (storageAvailable()) {
+        loadStorage();
+    } else {
+        next();
     }
+    display();
 };
 
 var next = function () {
-    displaySetCount(setCount++);
-    displayExercise(nextExercise(currentGroupIndex));
+    setCount++;
+    currentExercise = nextExercise(currentGroupIndex);
     incrementCurrentGroupIndex();
+    saveStorage();
+    display();
 };
 
 var nextExercise = function (currentGroupIndex) {
@@ -54,16 +75,58 @@ var getExerciseFromOption = function (option) {
     return {name: option.name, repCount: repCount};
 };
 
-var displayExercise = function (exercise) {
-    document.getElementById('exerciseName').value = exercise.name;
-    document.getElementById('repCount').value = exercise.repCount;
-};
-
 var incrementCurrentGroupIndex = function () {
     currentGroupIndex++;
     currentGroupIndex %= routine.length;
 };
 
-var displaySetCount = function (setCount) {
+var display = function () {
+    displayExercise();
+    displaySetCount();
+};
+
+var displayExercise = function () {
+    document.getElementById('exerciseName').value = currentExercise.name;
+    document.getElementById('repCount').value = currentExercise.repCount;
+};
+
+var displaySetCount = function () {
     document.getElementById('setCount').innerText = setCount;
+};
+
+var clear = function () {
+    currentGroupIndex = 0;
+    setCount = 0;
+    localStorage.clear();
+    next();
+    display();
+};
+
+var storageGroupIndexName = 'groupIndex';
+var storageSetCountName = 'setCount';
+var storageExerciseName = 'exercise';
+
+var storageAvailable = function () {
+    return Boolean(localStorage.getItem(storageExerciseName));
+};
+
+var loadStorage = function () {
+    var storageGroupIndex = localStorage.getItem(storageGroupIndexName);
+    if (storageGroupIndex) {
+        currentGroupIndex = parseInt(storageGroupIndex);
+    }
+    var storageSetCount = localStorage.getItem(storageSetCountName);
+    if (storageSetCount) {
+        setCount = parseInt(storageSetCount);
+    }
+    var storageExercise = localStorage.getItem(storageExerciseName);
+    if (storageExercise) {
+        currentExercise = JSON.parse(storageExercise);
+    }
+};
+
+var saveStorage = function () {
+    localStorage.setItem(storageGroupIndexName, currentGroupIndex);
+    localStorage.setItem(storageSetCountName, setCount);
+    localStorage.setItem(storageExerciseName, JSON.stringify(currentExercise));
 };
