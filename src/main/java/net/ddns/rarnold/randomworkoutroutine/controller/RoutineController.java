b3feb9179b9ff9ca724efe2f8de1fa1b363f2ee6
Rafe Arnold <rafe.arnold@gmail.com>
2020-04-13 20:35:38 +0100

    Delete ExerciseController and ExerciseService. Add RoutineController and RoutineService.

---
+++ src/main/java/net/ddns/rarnold/randomworkoutroutine/controller/RoutineController.java
@@ -0,0 +1,45 @@
+package net.ddns.rarnold.randomworkoutroutine.controller;
+
+import lombok.RequiredArgsConstructor;
+import net.ddns.rarnold.randomworkoutroutine.model.Exercise;
+import net.ddns.rarnold.randomworkoutroutine.model.Routine;
+import net.ddns.rarnold.randomworkoutroutine.service.RoutineService;
+import net.ddns.rarnold.randomworkoutroutine.session.RoutineSession;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("/routine")
+@RequiredArgsConstructor
+public class RoutineController {
+
+    private final RoutineService routineService;
+
+    @PostMapping("/save")
+    public void save(Routine routine) {
+        routineService.save(routine);
+    }
+
+    @PostMapping("/start/{name}")
+    public Exercise start(HttpSession httpSession, @PathVariable String name) {
+        httpSession.setAttribute(RoutineSession.ROUTINE_SESSION_ATTRIBUTE_NAME, new RoutineSession(routineService.getByName(name)));
+        return nextExercise(httpSession);
+    }
+
+    @PostMapping("/next")
+    public Exercise nextExercise(HttpSession httpSession) {
+        RoutineSession routineSession = getRoutineSession(httpSession);
+        return routineSession.nextExercise();
+    }
+
+    @GetMapping("/set-count")
+    public int getSetCount(HttpSession httpSession) {
+        RoutineSession routineSession = getRoutineSession(httpSession);
+        return routineSession.getSetCount();
+    }
+
+    private static RoutineSession getRoutineSession(HttpSession httpSession) {
+        return (RoutineSession) httpSession.getAttribute(RoutineSession.ROUTINE_SESSION_ATTRIBUTE_NAME);
+    }
+}
---
+++ src/main/java/net/ddns/rarnold/randomworkoutroutine/service/RoutineService.java
@@ -0,0 +1,27 @@
+package net.ddns.rarnold.randomworkoutroutine.service;
+
+import lombok.RequiredArgsConstructor;
+import net.ddns.rarnold.randomworkoutroutine.model.Routine;
+import net.ddns.rarnold.randomworkoutroutine.repository.RoutineRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class RoutineService {
+
+    private final RoutineRepository repository;
+
+    public Routine getByName(String name) {
+        Optional<Routine> routine = repository.findById(name);
+        if (!routine.isPresent()) {
+            throw new IllegalArgumentException("No routine by the name '" + name + "' exists");
+        }
+        return routine.get();
+    }
+
+    public void save(Routine routine) {
+        repository.save(routine);
+    }
+}
