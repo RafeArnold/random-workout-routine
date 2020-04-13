588136e231fe32565d28de40b49c43ebe25bcc9c
Rafe Arnold <rafe.arnold@gmail.com>
2020-04-13 16:43:54 +0100

    Add repository components

--- src/main/java/net/ddns/rarnold/randomworkoutroutine/repository/GroupRepository.java
+++ src/main/java/net/ddns/rarnold/randomworkoutroutine/repository/GroupRepository.java
@@ -1,9 +1,9 @@
 package net.ddns.rarnold.randomworkoutroutine.repository;
 
-import net.ddns.rarnold.randomworkoutroutine.model.ExerciseOption;
+import net.ddns.rarnold.randomworkoutroutine.model.Group;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ExerciseOptionRepository extends CrudRepository<ExerciseOption, String> {
+public interface GroupRepository extends CrudRepository<Group, Group.GroupId> {
 }
--- src/main/java/net/ddns/rarnold/randomworkoutroutine/repository/RoutineRepository.java
+++ src/main/java/net/ddns/rarnold/randomworkoutroutine/repository/RoutineRepository.java
@@ -1,9 +1,9 @@
 package net.ddns.rarnold.randomworkoutroutine.repository;
 
-import net.ddns.rarnold.randomworkoutroutine.model.ExerciseOption;
+import net.ddns.rarnold.randomworkoutroutine.model.Routine;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ExerciseOptionRepository extends CrudRepository<ExerciseOption, String> {
+public interface RoutineRepository extends CrudRepository<Routine, String> {
 }
