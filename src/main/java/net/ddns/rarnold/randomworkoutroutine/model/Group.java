b20976bbc3fed4d42ae7ce8991bf5869516afd95
Rafe Arnold <rafe.arnold@gmail.com>
2020-04-13 16:40:49 +0100

    Add constructors to Group and Group.GroupId

--- src/main/java/net/ddns/rarnold/randomworkoutroutine/model/Group.java
+++ src/main/java/net/ddns/rarnold/randomworkoutroutine/model/Group.java
@@ -1,6 +1,8 @@
 package net.ddns.rarnold.randomworkoutroutine.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -8,6 +10,7 @@ import java.util.List;
 
 @Entity(name = "grouping")
 @Data
+@NoArgsConstructor
 public class Group {
     @EmbeddedId
     private GroupId id;
@@ -19,8 +22,16 @@ public class Group {
     @ManyToMany
     private List<ExerciseOption> exerciseOptions;
 
+    public Group(Routine routine, int index, List<ExerciseOption> exerciseOptions) {
+        this.id = new GroupId(routine.getName(), index);
+        this.routine = routine;
+        this.exerciseOptions = exerciseOptions;
+    }
+
     @Embeddable
     @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class GroupId implements Serializable {
         private String routineName;
         private int index;
