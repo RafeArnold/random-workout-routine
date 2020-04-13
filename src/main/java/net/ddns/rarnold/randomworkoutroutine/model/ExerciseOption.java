3fe5e7deeb9565997938b651afe305a9bfff7347
Rafe Arnold <rafe.arnold@gmail.com>
2020-04-13 16:42:27 +0100

    Add ExerciseOption.getExercise convenience method

--- src/main/java/net/ddns/rarnold/randomworkoutroutine/model/ExerciseOption.java
+++ src/main/java/net/ddns/rarnold/randomworkoutroutine/model/ExerciseOption.java
@@ -3,6 +3,7 @@ package net.ddns.rarnold.randomworkoutroutine.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import net.ddns.rarnold.randomworkoutroutine.util.RandomUtils;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -16,4 +17,8 @@ public class ExerciseOption {
     private String name;
     private int repCountLowerBound;
     private int repCountUpperBound;
+
+    public Exercise getExercise() {
+        return new Exercise(name, RandomUtils.nextInt(repCountLowerBound, repCountUpperBound));
+    }
 }
