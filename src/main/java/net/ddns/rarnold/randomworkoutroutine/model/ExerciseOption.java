fee87e78cf75e409f69897c3bf538ecde172ca0b
Rafe Arnold <rafe.arnold@gmail.com>
2020-04-13 16:42:00 +0100

    Add constructors to ExerciseOption

--- src/main/java/net/ddns/rarnold/randomworkoutroutine/model/ExerciseOption.java
+++ src/main/java/net/ddns/rarnold/randomworkoutroutine/model/ExerciseOption.java
@@ -1,12 +1,16 @@
 package net.ddns.rarnold.randomworkoutroutine.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ExerciseOption {
     @Id
     private String name;
