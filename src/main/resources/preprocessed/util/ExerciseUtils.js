63af31731295840297f182419489786867d15cd1
Rafe Arnold <rafe.arnold@gmail.com>
2020-04-14 20:00:50 +0100

    Update next exercise url in ExerciseUtils.js

--- src/main/resources/preprocessed/util/ExerciseUtils.js
+++ src/main/resources/preprocessed/util/ExerciseUtils.js
@@ -1,6 +1,6 @@
 const getNextExercise = (onSuccess) => {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:8080/exercise/next');
+    xhr.open('POST', 'http://localhost:8080/routine/next');
     xhr.onload = () => onSuccess(JSON.parse(xhr.responseText));
     xhr.send();
 };
