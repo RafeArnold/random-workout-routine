ebda8315d86cd4013571622b6516cb7f58245573
Rafe Arnold <rafe.arnold@gmail.com>
2020-04-14 20:24:01 +0100

    Amend export of getNextExercise in RoutineUtils.js

--- src/main/resources/preprocessed/util/RoutineUtils.js
+++ src/main/resources/preprocessed/util/RoutineUtils.js
@@ -1,8 +1,6 @@
-const getNextExercise = (onSuccess) => {
+export function getNextExercise(onSuccess) {
     const xhr = new XMLHttpRequest();
     xhr.open('POST', 'http://localhost:8080/routine/next');
     xhr.onload = () => onSuccess(JSON.parse(xhr.responseText));
     xhr.send();
-};
-
-export default getNextExercise;
+}
