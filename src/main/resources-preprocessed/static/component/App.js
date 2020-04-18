974de62b398c72c41307521ce094739e79aac67c
Rafe Arnold <rafe.arnold@gmail.com>
2020-04-18 21:30:53 +0100

    Add react-router-dom. Set up basic routing. Add placeholder Edit component.

--- src/main/resources-preprocessed/static/component/App.js
+++ src/main/resources-preprocessed/static/component/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Edit from "./Edit";
 import Routine from "./Routine";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 class App extends React.Component {
     render() {
-        return <Routine/>;
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path='/edit'>
+                        <Edit/>
+                    </Route>
+                    <Route path='/routine'>
+                        <Routine/>
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        );
     }
 }
 
