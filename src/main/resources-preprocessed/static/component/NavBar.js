68af3cd14b659982e6ce9de5f87f8e6f46bcacf3
Rafe Arnold <rafe.arnold@gmail.com>
2020-04-18 22:44:38 +0100

    Add styling to NavBar component

--- src/main/resources-preprocessed/static/component/NavBar.js
+++ src/main/resources-preprocessed/static/component/NavBar.js
@@ -4,16 +4,17 @@ import {NavLink} from "react-router-dom";
 class NavBar extends React.Component {
     render() {
         return (
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+                <div className="navbar-brand">RWR</div>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <NavLink to="/" className="nav-link" exact>Home</NavLink>
                     </li>
-                    <li>
-                        <NavLink to="/routine">Start a Routine</NavLink>
+                    <li className="nav-item">
+                        <NavLink to="/routine" className="nav-link">Start a Routine</NavLink>
                     </li>
-                    <li>
-                        <NavLink to="/edit">Edit a Routine</NavLink>
+                    <li className="nav-item">
+                        <NavLink to="/edit" className="nav-link">Edit a Routine</NavLink>
                     </li>
                 </ul>
             </nav>
