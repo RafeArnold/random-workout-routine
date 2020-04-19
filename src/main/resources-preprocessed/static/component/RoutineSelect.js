846993db042da941b6eaf12527d8c4c8bb11804a
Rafe Arnold <rafe.arnold@gmail.com>
2020-04-19 21:46:51 +0100

    Add functionality to RoutineSelect component

--- src/main/resources-preprocessed/static/component/RoutineSelect.js
+++ src/main/resources-preprocessed/static/component/RoutineSelect.js
@@ -1,9 +1,46 @@
 import React from "react";
+import {continueRoutinePath, getRoutineNames, startRoutine} from "../util/RoutineUtils";
+
+const selectId = "routineSelect";
 
 class RoutineSelect extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {options: null};
+        this.mapNamesToOptions = this.mapNamesToOptions.bind(this);
+        this.start = this.start.bind(this);
+    }
+
+    componentDidMount() {
+        getRoutineNames((names) => this.setState({options: this.mapNamesToOptions(names)}));
+    }
+
+    mapNamesToOptions(routineNames) {
+        return routineNames.map((name) => (
+            <option key={name} value={name}>{name}</option>
+        ));
+    }
+
+    start() {
+        const routineName = document.getElementById(selectId).value;
+        if (routineName) {
+            startRoutine(routineName, () => window.location.href = continueRoutinePath);
+        }
+    }
+
     render() {
+        const options = this.state.options;
+        const selectSize = options ? Math.min(options.length, 5) : 1;
         return (
-            <h1>Select a Routine</h1>
+            <>
+                <h1 className="my-3">Select a Routine</h1>
+                <div className="row">
+                    <div className="col-8 col-md-6 col-lg-5 form-group">
+                        <select id={selectId} className="form-control" size={selectSize}>{options}</select>
+                    </div>
+                </div>
+                <button className="btn btn-dark" onClick={this.start}>Start</button>
+            </>
         );
     }
 }
--- src/main/resources-preprocessed/static/util/RoutineUtils.js
+++ src/main/resources-preprocessed/static/util/RoutineUtils.js
@@ -3,10 +3,24 @@ export const editRoutinePath = "/edit";
 export const newRoutinePath = "/new-routine";
 export const continueRoutinePath = "/routine";
 
+export function getRoutineNames(onSuccess) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost:8080" + apiRoutinePath + "/names");
+    xhr.onload = () => onSuccess(JSON.parse(xhr.responseText));
+    xhr.send();
+}
+
 export function routineIsActive(onSuccess) {
     const xhr = new XMLHttpRequest();
     xhr.open("GET", "http://localhost:8080" + apiRoutinePath + "/in-progress");
     xhr.onload = () => onSuccess(xhr.responseText === "true");
+    xhr.send();
+}
+
+export function startRoutine(routineName, onSuccess) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:8080" + apiRoutinePath + "/start/" + routineName);
+    xhr.onload = onSuccess;
     xhr.send();
 }
 
