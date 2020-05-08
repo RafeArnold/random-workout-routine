794a3a8016a1be763aab5f854552be136c130c36
Rafe Arnold <rafe.arnold@gmail.com>
2020-05-08 20:31:41 +0100

    Stop DataLoader overwriting existing entities.

--- src/main/java/net/ddns/rarnold/randomworkoutroutine/init/DataLoader.java
+++ src/main/java/net/ddns/rarnold/randomworkoutroutine/init/DataLoader.java
@@ -24,23 +24,25 @@ public class DataLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        List<ExerciseOption> options = new ArrayList<>();
-        options.add(new ExerciseOption("sit ups", 8, 25));
-        options.add(new ExerciseOption("push ups", 5, 20));
-        options.add(new ExerciseOption("tricep dips", 10, 25));
-        options.add(new ExerciseOption("rope climbs", 1, 5));
-        options.add(new ExerciseOption("monkey bars", 1, 4));
-        options.add(new ExerciseOption("bicep curls", 8, 26));
-        for (ExerciseOption option : options) {
-            optionRepository.save(option);
+        if (optionRepository.count() == 0 && groupRepository.count() == 0 && routineRepository.count() == 0) {
+            List<ExerciseOption> options = new ArrayList<>();
+            options.add(new ExerciseOption("sit ups", 8, 25));
+            options.add(new ExerciseOption("push ups", 5, 20));
+            options.add(new ExerciseOption("tricep dips", 10, 25));
+            options.add(new ExerciseOption("rope climbs", 1, 5));
+            options.add(new ExerciseOption("monkey bars", 1, 4));
+            options.add(new ExerciseOption("bicep curls", 8, 26));
+            for (ExerciseOption option : options) {
+                optionRepository.save(option);
+            }
+            List<Group> groups = new ArrayList<>();
+            groups.add(new Group("A", Arrays.asList(options.get(0), options.get(1), options.get(2))));
+            groups.add(new Group("B", Arrays.asList(options.get(3), options.get(4), options.get(5))));
+            for (Group group : groups) {
+                groupRepository.save(group);
+            }
+            Routine routine = new Routine("mix", groups);
+            routineRepository.save(routine);
         }
-        List<Group> groups = new ArrayList<>();
-        groups.add(new Group("A", Arrays.asList(options.get(0), options.get(1), options.get(2))));
-        groups.add(new Group("B", Arrays.asList(options.get(3), options.get(4), options.get(5))));
-        for (Group group : groups) {
-            groupRepository.save(group);
-        }
-        Routine routine = new Routine("mix", groups);
-        routineRepository.save(routine);
     }
 }
