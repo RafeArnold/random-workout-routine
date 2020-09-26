e815e30fa6b2ee23fc823f2e1bb0910a5d3a9d42
Rafe Arnold <rafe.arnold@gmail.com>
2020-09-26 15:30:00 +0100

    Add tags field to Item.

--- src/main/java/net/ddns/rarnold/randomworkoutroutine/model/entity/Item.java
+++ src/main/java/net/ddns/rarnold/randomworkoutroutine/model/entity/Item.java
@@ -3,7 +3,10 @@ package net.ddns.rarnold.randomworkoutroutine.model.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
@@ -16,4 +19,19 @@ public abstract class Item {
 
     @Column(unique = true, nullable = false)
     protected String name;
+
+    @ElementCollection
+    protected Set<String> tags;
+
+    /**
+     * Sets {@link #tags}. Removes blank strings, trims and converts remaining strings to lowercase
+     * and converts to an unmodifiable set.
+     */
+    public void setTags(Collection<String> tags) {
+        this.tags = tags.stream()
+                .filter(tag -> !tag.isBlank())
+                .map(String::trim)
+                .map(String::toLowerCase)
+                .collect(Collectors.toUnmodifiableSet());
+    }
 }
