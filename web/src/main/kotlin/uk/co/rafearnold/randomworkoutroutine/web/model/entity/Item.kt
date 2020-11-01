9d24d869c36e434ad5a53c3574125042b4e4a386
Rafe Arnold <rafe.arnold@gmail.com>
2020-11-01 13:18:39 +0000

    Update set function of Item.tags property.

--- web/src/main/kotlin/uk/co/rafearnold/randomworkoutroutine/web/model/entity/Item.kt
+++ web/src/main/kotlin/uk/co/rafearnold/randomworkoutroutine/web/model/entity/Item.kt
@@ -2,7 +2,6 @@ package uk.co.rafearnold.randomworkoutroutine.web.model.entity
 
 import uk.co.rafearnold.randomworkoutroutine.model.Item
 import java.util.*
-import java.util.stream.Collectors
 import javax.persistence.*
 
 @Entity
@@ -16,14 +15,13 @@ abstract class Item(
     @ElementCollection
     override var tags: MutableSet<String> = tags
         /**
-         * Sets [.tags]. Removes blank strings, trims and converts remaining strings to lowercase
-         * and converts to an unmodifiable set.
+         * Removes blank strings, trims and converts remaining strings to lowercase.
          */
         set(value) {
-            field = value.stream()
-                    .filter { tag: String -> !tag.isBlank() }
-                    .map { obj: String -> obj.trim { it <= ' ' } }
-                    .map { obj: String -> obj.toLowerCase() }
-                    .collect(Collectors.toUnmodifiableSet())
+            field = value
+                .filter { tag: String -> !tag.isBlank() }
+                .map { obj: String -> obj.trim() }
+                .map { obj: String -> obj.toLowerCase() }
+                .toMutableSet()
         }
 }
