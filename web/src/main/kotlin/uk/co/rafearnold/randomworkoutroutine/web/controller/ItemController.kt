7687b3fe45ce42df208c1c15ad1d54f72b7cfebe
Rafe Arnold <rafe.arnold@gmail.com>
2020-11-15 20:43:57 +0000

    Add better error responses to ItemController.

--- web/src/main/kotlin/uk/co/rafearnold/randomworkoutroutine/web/controller/ItemController.kt
+++ web/src/main/kotlin/uk/co/rafearnold/randomworkoutroutine/web/controller/ItemController.kt
@@ -1,21 +1,42 @@
 package uk.co.rafearnold.randomworkoutroutine.web.controller
 
+import org.hibernate.exception.ConstraintViolationException
+import org.springframework.dao.DataIntegrityViolationException
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import uk.co.rafearnold.randomworkoutroutine.model.SimpleItem
 import uk.co.rafearnold.randomworkoutroutine.web.model.entity.Item
 import uk.co.rafearnold.randomworkoutroutine.web.service.ItemService
 import java.util.*
+import javax.servlet.http.HttpServletResponse
 
 abstract class ItemController<T : Item>(protected val service: ItemService<T>) {
 
-    // TODO: 404 when item doesn't exist.
     @GetMapping("/{id}")
-    fun getById(@PathVariable id: UUID): Optional<T> = service.getById(id)
+    fun getById(@PathVariable id: UUID, response: HttpServletResponse): Optional<T> {
+        val optional: Optional<T> = service.getById(id)
+        if (optional.isEmpty) {
+            response.sendError(HttpStatus.NOT_FOUND.value(), "No item with ID '$id' found")
+        }
+        return optional
+    }
 
-    // TODO: Better response status when an existing item is provided without its ID.
-    // TODO: Don't allow items with blank names.
     @PostMapping("/save")
-    fun save(@RequestBody option: T) = service.save(option)
+    fun save(@RequestBody item: T, response: HttpServletResponse) {
+        if (item.name.isBlank()) {
+            response.sendError(HttpStatus.BAD_REQUEST.value(), "Item name must not be blank")
+            return
+        }
+        try {
+            service.save(item)
+        } catch (e: DataIntegrityViolationException) {
+            // Check if it's a unique constraint violation.
+            val cause = e.cause
+            if (cause is ConstraintViolationException && cause.sqlState == "23505") {
+                response.sendError(HttpStatus.CONFLICT.value(), cause.cause?.message)
+            }
+        }
+    }
 
     @DeleteMapping("/delete/{id}")
     fun delete(@PathVariable id: UUID) = service.getById(id).ifPresent { item: T -> service.delete(item) }
